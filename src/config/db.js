bfbdbc9f0324c6f8b4646ed167f266359a54db6e
Matias Silvera <matusilvera808@gmail.com>
2025-06-23 09:57:25 -0300

    Se ha añadido la configuracion inicial para conectarse a la dd, testear la conexion y sincronizar los modelos

---
+++ src/config/db.js
@@ -0,0 +1,13 @@
+import sequelize from "./database";
+
+const startDB = async()=>{
+    try{
+        await sequelize.authenticate();
+        console.log("Conectado a la base de datos con exito.");
+        await sequelize.sync();
+    }catch(err){
+        console.log("Hubo un problema al conectarse con la base de datos: ",);
+    }
+}
+
+export default startDB;
